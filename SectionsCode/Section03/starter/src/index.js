854dd8867016556f12b0bd0050923a2634c5968e
unknown <grigorischneider@gmail.com>
2023-01-17 19:30:18 +0100

    dispatching the acitons & making action types & subscribe & unsubscribe

--- SectionsCode/Section03/starter/src/index.js
+++ SectionsCode/Section03/starter/src/index.js
@@ -1,3 +1,14 @@
 import store from "./store";
+import { addTask, removeTask } from "./action";
 
+const unsubscribe = store.subscribe(() => {
+  console.log("Updated", store.getState());
+});
+
+store.dispatch(addTask("Task 1"));
+console.log(store.getState());
+
+unsubscribe();
+
+store.dispatch(removeTask(1));
 console.log(store.getState());
