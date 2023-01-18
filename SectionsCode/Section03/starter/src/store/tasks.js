801ef3129caa64c7b3d8abc21952adb5c312901e
unknown <grigorischneider@gmail.com>
2023-01-18 09:22:39 +0100

    Implementing Duck Module

--- SectionsCode/Section03/starter/src/store/tasks.js
+++ SectionsCode/Section03/starter/src/store/tasks.js
@@ -1,10 +1,30 @@
-import * as actionTypes from "./actionTypes";
+// Action Types
+const ADD_TASK = "ADD_TASK";
+const REMOVE_TASK = "REMOVE_TASK";
+const TASK_COMPLETED = "TASK_COMPLETED";
 
+// Actions
+export const addTask = (task) => {
+  return { type: ADD_TASK, payload: { task: task } };
+};
+
+export const removeTask = (id) => {
+  return { type: REMOVE_TASK, payload: { id: id } };
+};
+
+export const completedTask = (id) => {
+  return {
+    type: TASK_COMPLETED,
+    payload: { id: id },
+  };
+};
+
+// Reducer
 let id = 0;
 
 export default function reducer(state = [], action) {
   switch (action.type) {
-    case actionTypes.ADD_TASK:
+    case ADD_TASK:
       return [
         ...state,
         {
@@ -13,10 +33,10 @@ export default function reducer(state = [], action) {
           completed: false,
         },
       ];
-    case actionTypes.REMOVE_TASK:
+    case REMOVE_TASK:
       return state.filter((task) => task.id !== action.payload.id);
 
-    case actionTypes.TASK_COMPLETED:
+    case TASK_COMPLETED:
       return state.map((task) =>
         task.id === action.payload.id
           ? {
