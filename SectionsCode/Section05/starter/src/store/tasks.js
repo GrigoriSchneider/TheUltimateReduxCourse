206cb6f9738beba00c98d67f11e056ed14a36594
unknown <grigorischneider@gmail.com>
2023-01-19 18:21:32 +0100

    Creating slices with toolkit &  Combine Reducers

--- SectionsCode/Section05/starter/src/store/tasks.js
+++ SectionsCode/Section05/starter/src/store/tasks.js
@@ -1,58 +1,29 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-// Actions
-export const addTask = createAction("ADD_TASK");
-export const removeTask = createAction("REMOVE_TASK");
-export const completedTask = createAction("TASK_COMPLETED");
-console.log(addTask.type);
-
-// Reducer
 let id = 0;
 
-export default createReducer([], {
-  [addTask.type]: (state, action) => {
-    state.push({
-      id: ++id,
-      task: action.payload.task,
-      completed: false,
-    });
-  },
-  [removeTask.type]: (state, action) => {
-    const index = state.findIndex((task) => task.id === action.payload.id);
-    state.splice(index, 1);
-  },
-  [completedTask.type]: (state, action) => {
-    const index = state.findIndex((task) => task.id === action.payload.id);
-    state[index].completed = true;
+const taskSlice = createSlice({
+  name: "tasks",
+  initialState: [],
+  reducers: {
+    // action: function
+    addTask: (state, action) => {
+      state.push({
+        id: ++id,
+        task: action.payload.task,
+        completed: false,
+      });
+    },
+    removeTask: (state, action) => {
+      const index = state.findIndex((task) => task.id === action.payload.id);
+      state.splice(index, 1);
+    },
+    completedTask: (state, action) => {
+      const index = state.findIndex((task) => task.id === action.payload.id);
+      state[index].completed = true;
+    },
   },
 });
 
-// export default function reducer(state = [], action) {
-//   switch (action.type) {
-//     case addTask.type:
-//       return [
-//         ...state,
-//         {
-//           id: ++id,
-//           task: action.payload.task,
-//           completed: false,
-//         },
-//       ];
-
-//     case removeTask.type:
-//       return state.filter((task) => task.id !== action.payload.id);
-
-//     case completedTask.type:
-//       return state.map((task) =>
-//         task.id === action.payload.id
-//           ? {
-//               ...task,
-//               completed: true,
-//             }
-//           : task
-//       );
-
-//     default:
-//       return state;
-//   }
-// }
+export const { addTask, removeTask, completedTask } = taskSlice.actions;
+export default taskSlice.reducer;
