222b9c1098f0443683ca4323a844110aa4dd86e2
unknown <grigorischneider@gmail.com>
2023-01-20 11:48:24 +0100

    redux logger

--- SectionsCode/Section05/finish/src/store/employees.js
+++ SectionsCode/Section05/finish/src/store/employees.js
@@ -2,13 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 let id = 0;
 
 const employeeSlice = createSlice({
-    name: "employees",
-    initialState: [],
-    reducers: {
-        addEmployee: (state, action) => {
-            state.push({ id: ++id, name: action.payload.name });
-        },
+  name: "employees",
+  initialState: [],
+  reducers: {
+    addEmployee: (state, action) => {
+      state.push({ id: ++id, name: action.payload.name });
     },
+  },
 });
 
 export const { addEmployee } = employeeSlice.actions;
--- SectionsCode/Section06/start/src/store/configureStore.js
+++ SectionsCode/Section06/start/src/store/configureStore.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import employeeReducer from "./employees";
 import taskReducer from "./tasks";
+import logger from "redux-logger";
 
 const store = configureStore({
-    reducer: {
-        tasks: taskReducer,
-        employees: employeeReducer,
-    },
+  reducer: {
+    tasks: taskReducer,
+    employees: employeeReducer,
+  },
+  middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), logger],
 });
 
 export default store;
