1fec9d2867d650bb892c5b7e8c3e9844b464f23d
unknown <grigorischneider@gmail.com>
2023-01-21 18:39:18 +0100

    createAsyncThnuk Method

--- SectionsCode/Section07/start/src/index.js
+++ SectionsCode/Section07/start/src/index.js
@@ -1,17 +1,19 @@
 import store from "./store/configureStore";
 import axios from "axios";
-import { getTasks } from "./store/tasks";
+import { fetchTasks } from "./store/tasks";
 
-const gettingTasks = async () => {
-  try {
-    // calling api
-    const response = await axios.get("http://localhost:5000/api/tasks");
-    console.log(response);
-    // dispatch action
-    store.dispatch(getTasks({ tasks: response.data }));
-  } catch (error) {
-    store.dispatch({ type: "SHOW_ERROR", payload: { error: error.message } });
-  }
-};
+// const gettingTasks = async () => {
+//   try {
+//     // calling api
+//     const response = await axios.get("http://localhost:5000/api/tasks");
+//     console.log(response);
+//     // dispatch action
+//     store.dispatch(getTasks({ tasks: response.data }));
+//   } catch (error) {
+//     store.dispatch({ type: "SHOW_ERROR", payload: { error: error.message } });
+//   }
+// };
 
-gettingTasks();
+// gettingTasks();
+
+store.dispatch(fetchTasks());
