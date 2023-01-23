7661a663de3272ee89975eefd90dddf5aa15a1ff
unknown <grigorischneider@gmail.com>
2023-01-23 14:06:54 +0100

    configure store in react

---
+++ SectionsCode/Section08/start/to-do-app/src/components/Tasks.js
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import StoreContext from "../contexts/storeContext";
+
+const Tasks = () => {
+  const store = useContext(StoreContext);
+  console.log(store);
+  return <div>Tasks</div>;
+};
+
+export default Tasks;
