dd64048fcb20f5afdc8ccb72ada7844d3923589a
unknown <grigorischneider@gmail.com>
2023-01-24 13:59:35 +0100

    addTask

--- SectionsCode/Section08/start/to-do-app/src/App.js
+++ SectionsCode/Section08/start/to-do-app/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 
 const App = () => {
   return (
     <div>
+      <AddTask />
       <Tasks />
     </div>
   );
